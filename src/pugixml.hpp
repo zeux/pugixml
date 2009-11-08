3cf6af642856382c75467b41dc311778c3afc1fe
arseny.kapoulkine <arseny.kapoulkine@99668b35-9821-0410-8761-19e4c4f06640>
2009-11-08 19:16:43 +0000

    docs: Minor documentation changes
    git-svn-id: http://pugixml.googlecode.com/svn/trunk@233 99668b35-9821-0410-8761-19e4c4f06640

--- src/pugixml.hpp
+++ src/pugixml.hpp
@@ -262,7 +262,7 @@ namespace pugi
 	enum xpath_type_t
 	{
 		xpath_type_none,      ///< Unknown type (query failed to compile)
-		xpath_type_node_set,  ///< Node set (\see xpath_node_set)
+		xpath_type_node_set,  ///< Node set (xpath_node_set)
 		xpath_type_number,    ///< Number
 		xpath_type_string,    ///< String
 		xpath_type_boolean    ///< Boolean
