fa16efd4389baf62d7ca9b3491cd1e39cc9d4952
arseny.kapoulkine <arseny.kapoulkine@99668b35-9821-0410-8761-19e4c4f06640>
2010-07-06 11:22:45 +0000

    docs: Added basic traversal sample and some document reading documentation
    git-svn-id: http://pugixml.googlecode.com/svn/trunk@560 99668b35-9821-0410-8761-19e4c4f06640

---
+++ docs/samples/traverse_base.cpp
@@ -0,0 +1,54 @@
+#include "pugixml.hpp"
+
+#include <string.h>
+#include <iostream>
+
+int main()
+{
+	pugi::xml_document doc;
+    if (!doc.load_file("xgconsole.xml")) return -1;
+
+    pugi::xml_node tools = doc.child("Profile").child("Tools");
+
+    //[code_traverse_base_basic
+    for (pugi::xml_node tool = tools.first_child(); tool; tool = tool.next_sibling())
+    {
+        std::cout << "Tool:";
+
+        for (pugi::xml_attribute attr = tool.first_attribute(); attr; attr = attr.next_attribute())
+        {
+            std::cout << " " << attr.name() << "=" << attr.value();
+        }
+
+        std::cout << std::endl;
+    }
+    //]
+
+    std::cout << std::endl;
+
+    //[code_traverse_base_data
+    for (pugi::xml_node tool = tools.child("Tool"); tool; tool = tool.next_sibling("Tool"))
+    {
+        std::cout << "Tool " << tool.attribute("Filename").value();
+        std::cout << ": AllowRemote " << tool.attribute("AllowRemote").as_bool();
+        std::cout << ", Timeout " << tool.attribute("Timeout").as_int();
+        std::cout << ", Description '" << tool.child_value("Description") << "'\n";
+    }
+    //]
+
+    std::cout << std::endl;
+
+    //[code_traverse_base_contents
+    for (pugi::xml_node tool = tools.child("Tool"); tool; tool = tool.next_sibling("Tool"))
+    {
+        std::cout << "Tool " << tool.attribute("Filename").value() << ":";
+
+        for (pugi::xml_attribute attr = tool.first_attribute(); attr; attr = attr.next_attribute())
+        {
+            if (strcmp(attr.name(), "Filename") != 0) std::cout << " " << attr.name() << "=" << attr.value();
+        }
+
+        std::cout << std::endl;
+    }
+    //]
+}
