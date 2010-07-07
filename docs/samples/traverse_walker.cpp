813cabb5e67bab646a838890fa55363ad566ee11
arseny.kapoulkine <arseny.kapoulkine@99668b35-9821-0410-8761-19e4c4f06640>
2010-07-07 14:47:48 +0000

    docs: Added traverse samples, minor manual fixes, 'getting' section is almost finished
    git-svn-id: http://pugixml.googlecode.com/svn/trunk@567 99668b35-9821-0410-8761-19e4c4f06640

---
+++ docs/samples/traverse_walker.cpp
@@ -0,0 +1,33 @@
+#include "pugixml.hpp"
+
+#include <iostream>
+
+const char* node_types[] =
+{
+    "null", "document", "element", "pcdata", "cdata", "comment", "pi", "declaration"
+};
+
+//[code_traverse_walker_impl
+struct simple_walker: pugi::xml_tree_walker
+{
+    virtual bool for_each(pugi::xml_node& node)
+    {
+        for (int i = 0; i < depth(); ++i) std::cout << "  "; // indentation
+
+        std::cout << node_types[node.type()] << ": name='" << node.name() << "', value='" << node.value() << "'\n";
+
+        return true; // continue traversal
+    }
+};
+//]
+
+int main()
+{
+	pugi::xml_document doc;
+    if (!doc.load_file("tree.xml")) return -1;
+
+    //[code_traverse_walker_traverse
+    simple_walker walker;
+    doc.traverse(walker);
+    //]
+}
