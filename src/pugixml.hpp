e68048518ea4830ffcdaf79e7f929713263c6f06
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2015-03-18 21:19:58 -0700

    Update version to 1.6

--- src/pugixml.hpp
+++ src/pugixml.hpp
@@ -1,5 +1,5 @@
 /**
- * pugixml parser - version 1.5
+ * pugixml parser - version 1.6
  * --------------------------------------------------------
  * Copyright (C) 2006-2014, by Arseny Kapoulkine (arseny.kapoulkine@gmail.com)
  * Report bugs and download new versions at http://pugixml.org/
@@ -13,7 +13,7 @@
 
 #ifndef PUGIXML_VERSION
 // Define version macro; evaluates to major * 100 + minor so that it's safe to use in less-than comparisons
-#	define PUGIXML_VERSION 150
+#	define PUGIXML_VERSION 160
 #endif
 
 // Include user configuration file (this can define various configuration macros)
