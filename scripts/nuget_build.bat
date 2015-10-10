3147190f3d652419a7f33e8110b480f36603b1ed
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2015-10-10 11:42:25 -0700

    scripts: Cleanup NuGet packaging
    Move pugixml project file for VS2015 to scripts/ and unify the output file
    structure similar to old VS201x projects. Remove test projects and solution
    since they are not required for building.
    
    Provide more accurate information in the package script and handle build
    errors during package construction properly.

---
+++ scripts/nuget_build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+"%VS140COMNTOOLS%\VsMSBuildCmd.bat" && ^
+msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /v:minimal /nologo && ^
+msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x86 /v:minimal /nologo && ^
+msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /v:minimal /nologo && ^
+msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x64 /v:minimal /nologo && ^
+powershell Write-NuGetPackage nuget.autopkg
