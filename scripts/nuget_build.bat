97a6f65200bc61a5a5bec657a835f33ec902aab7
Stephen Daudell <s.daudell@boldwebservices.com>
2016-08-04 14:53:47 -0500

    Potential fix for build issues.

--- scripts/nuget_build.bat
+++ scripts/nuget_build.bat
@@ -1,13 +1,11 @@
 @echo off
 cd %~dp0
 
-"%VS120COMNTOOLS%\VsMSBuildCmd.bat" && ^
+"%VS140COMNTOOLS%\VsMSBuildCmd.bat" && ^
 msbuild pugixml_vs2013_static.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /v:minimal /nologo && ^
 msbuild pugixml_vs2013_static.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x86 /v:minimal /nologo && ^
 msbuild pugixml_vs2013_static.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /v:minimal /nologo && ^
-msbuild pugixml_vs2013_static.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x64 /v:minimal /nologo
-
-"%VS140COMNTOOLS%\VsMSBuildCmd.bat" && ^
+msbuild pugixml_vs2013_static.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x64 /v:minimal /nologo && ^
 msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /v:minimal /nologo && ^
 msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x86 /v:minimal /nologo && ^
 msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /v:minimal /nologo && ^
