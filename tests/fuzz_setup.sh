8b15ae801531f64804566391bfbd10db451956ca
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-02-08 08:48:33 -0800

    tests: Add a script to set up fuzzing tools
    This downloads a clang build that has support for instrumentation, and also
    downloads and compiles libFuzzer.a.

---
+++ tests/fuzz_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get --yes install subversion screen gcc g++ cmake ninja-build golang autoconf libtool apache2 python-dev pkg-config zlib1g-dev libgcrypt11-dev
+
+git clone https://chromium.googlesource.com/chromium/src/tools/clang
+clang/scripts/update.py
+sudo cp -rf third_party/llvm-build/Release+Asserts/lib/* /usr/local/lib/
+sudo cp -rf third_party/llvm-build/Release+Asserts/bin/* /usr/local/bin
+
+svn co http://llvm.org/svn/llvm-project/llvm/trunk/lib/Fuzzer
+Fuzzer/build.sh
