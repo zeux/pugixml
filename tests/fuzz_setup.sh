0eb1ddb975e867079e87e2345e59a520890b275a
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-04-03 20:36:33 -0700

    tests: Fix fuzz_setup.sh
    The script only worked if clang folder was already created.

--- tests/fuzz_setup.sh
+++ tests/fuzz_setup.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get --yes install subversion screen gcc g++ cmake ninja-build golang autoconf libtool apache2 python-dev pkg-config zlib1g-dev libgcrypt11-dev
 
+mkdir -p clang
 cd clang
 git clone https://chromium.googlesource.com/chromium/src/tools/clang
 cd ..
