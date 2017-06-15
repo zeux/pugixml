e9446237809c4c700d728e46c8bb5c04c6e99415
Ivan Gagis <igagis@gmail.com>
2017-06-15 11:17:34 +0300

    Appveyor image set to VS2017

--- scripts/nuget_build.bat
+++ scripts/nuget_build.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
 
-"%VS140COMNTOOLS%\VsMSBuildCmd.bat" && ^
+"%VS141COMNTOOLS%\VsMSBuildCmd.bat" && ^
 msbuild pugixml_vs2013_static.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /v:minimal /nologo && ^
 msbuild pugixml_vs2013_static.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x86 /v:minimal /nologo && ^
 msbuild pugixml_vs2013_static.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /v:minimal /nologo && ^
@@ -10,7 +10,6 @@ msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x86
 msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x86 /v:minimal /nologo && ^
 msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /v:minimal /nologo && ^
 msbuild pugixml_vs2015.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x64 /v:minimal /nologo && ^
-"%VS141COMNTOOLS%\VsMSBuildCmd.bat" && ^
 msbuild pugixml_vs2017.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /v:minimal /nologo && ^
 msbuild pugixml_vs2017.vcxproj /t:Rebuild /p:Configuration=Release /p:Platform=x86 /v:minimal /nologo && ^
 msbuild pugixml_vs2017.vcxproj /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /v:minimal /nologo && ^
