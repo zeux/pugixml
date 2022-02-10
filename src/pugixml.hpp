dd50fa5b45ab8d58d6c27566c2eaf04a8b7e5841
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2022-02-10 08:36:19 -0800

    Fix PUGIXML_VERSION macro
    Also make sure the line shows up in grep when using the current version
    number.
    
    Fixes #478.

--- src/pugixml.hpp
+++ src/pugixml.hpp
@@ -11,10 +11,10 @@
  * Copyright (C) 2003, by Kristen Wegner (kristen@tima.net)
  */
 
-#ifndef PUGIXML_VERSION
 // Define version macro; evaluates to major * 1000 + minor * 10 + patch so that it's safe to use in less-than comparisons
 // Note: pugixml used major * 100 + minor * 10 + patch format up until 1.9 (which had version identifier 190); starting from pugixml 1.10, the minor version number is two digits
-#	define PUGIXML_VERSION 1110
+#ifndef PUGIXML_VERSION
+#	define PUGIXML_VERSION 1120 // 1.12
 #endif
 
 // Include user configuration file (this can define various configuration macros)
